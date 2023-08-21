53e355c41158b2e7982e5991368e754085e496b3
Roman Telik <rtelik58@gmail.com>
2023-08-21 22:37:48 +0300

    Searchbar-refactoring

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,13 +7,6 @@ import { Button } from './Button';
 
 axios.defaults.baseURL = 'https://pixabay.com/api/';
 export const App = () => {
-  // state = {
-  //   pictures: [],
-  //   isLoading: false,
-  //   inputValue: '',
-  //   page: 1,
-  //   total: 0,
-  // };
   const [pictures, setPictures] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [inputValue, setInputValue] = useState('');
@@ -22,104 +15,46 @@ export const App = () => {
 
   const handleSubmit = submittedValue => {
     console.log('Значення, передане з дитячого компонента:', submittedValue);
-    // this.setState({ inputValue: submittedValue });
     setInputValue(submittedValue);
   };
   useEffect(() => {
-    // this.setState({ isLoading: true });
     setIsLoading(true);
     async function isLoadingAsy() {
       try {
         const response = await axios.get(
           `?q=${inputValue}&page=1&key=37812301-bb78e35e415e6149d67a423b2&image_type=photo&orientation=horizontal&per_page=12`
         );
-
-        // this.setState({
-        //   pictures: response.data.hits,
-        //   total: response.data.totalHits,
-        //   isLoading: false,
-        // });
         setPictures(response.data.hits);
         setTotal(response.data.totalHits);
         setIsLoading(false);
         console.log(response);
       } catch (error) {
         console.error('Error fetching data:', error);
-        // this.setState({ isLoading: false });
         setIsLoading(false);
       }
     }
     isLoadingAsy();
   }, [inputValue]);
   useEffect(() => {
-    // this.setState({ isLoading: true });
     setIsLoading(true);
     async function pageAsy() {
       try {
         const response = await axios.get(
           `?q=${inputValue}&page=${page}&key=37812301-bb78e35e415e6149d67a423b2&image_type=photo&orientation=horizontal&per_page=12`
         );
-        // this.setState(prevState => ({
-        //   pictures: [...prevState.pictures, ...response.data.hits],
-        //   isLoading: false,
-        // }));
         setPictures(prevPictures => [...prevPictures, ...response.data.hits]);
         setIsLoading(false);
-        // console.log(response);
       } catch (error) {
         console.error('Error fetching data:', error);
-        // this.setState({ isLoading: false });
         setIsLoading(false);
       }
     }
     pageAsy();
   }, [page, inputValue]);
-  // async componentDidUpdate(prevProps, prevState) {
-  //   if (prevState.inputValue !== this.state.inputValue) {
-  //     // this.setState({ isLoading: true });
-
-  //     // try {
-  //     //   const response = await axios.get(
-  //     //     `?q=${this.state.inputValue}&page=1&key=37812301-bb78e35e415e6149d67a423b2&image_type=photo&orientation=horizontal&per_page=12`
-  //     //   );
-
-  //     //   this.setState({
-  //     //     pictures: response.data.hits,
-  //     //     total: response.data.totalHits,
-  //     //     isLoading: false,
-  //     //   });
-  //     //   console.log(response);
-  //     // } catch (error) {
-  //     //   console.error('Error fetching data:', error);
-  //     //   this.setState({ isLoading: false });
-  //     // }
-  //   } else if (prevState.page !== this.state.page) {
-  //     const { inputValue, page } = this.state;
-
-  //     this.setState({ isLoading: true });
-
-  //     try {
-  //       const response = await axios.get(
-  //         `?q=${inputValue}&page=${page}&key=37812301-bb78e35e415e6149d67a423b2&image_type=photo&orientation=horizontal&per_page=12`
-  //       );
-  //       this.setState(prevState => ({
-  //         pictures: [...prevState.pictures, ...response.data.hits],
-  //         isLoading: false,
-  //       }));
-  //       // console.log(response);
-  //     } catch (error) {
-  //       console.error('Error fetching data:', error);
-  //       this.setState({ isLoading: false });
-  //     }
-  //   }
-  // }
   const loadMoreImages = () => {
-    // this.setState(prevState => ({ page: prevState.page + 1 }));
     setPage(prevPage => prevPage + 1);
     console.log(page);
   };
-  // render() {
-  //   const { pictures, isLoading, page } = this.state;
   const hasImages = pictures.length > 0;
   const totalPagesUnrounded = total / 12;
   const totalPages = Math.ceil(totalPagesUnrounded);
--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,52 +1,44 @@
-import React, { Component } from 'react';
-// import PropTypes from 'prop-types';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import axios from 'axios';
 
 axios.defaults.baseURL = 'https://pixabay.com/api/';
 
-export class SearchForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      // pictures: [],
-      // isLoading: false,
-      inputValue: '',
-      // page: 1,
-    };
-  }
-  handleChange = e => {
-    this.setState({ inputValue: e.target.value });
+export const SearchForm = ({ onFormSubmit }) => {
+  const [inputValue, setInputValue] = useState('');
+  const handleChange = e => {
+    setInputValue(e.target.value);
   };
-  handleSubmit = async evt => {
+  const handleSubmit = async evt => {
     evt.preventDefault();
-    console.log(`Search query: ${this.state.inputValue}`);
-    this.props.onFormSubmit(this.state.inputValue);
+    console.log(`Search query: ${inputValue}`);
+    onFormSubmit(inputValue);
   };
-
-  render() {
-    return (
-      <header
-        className="searchbar"
-        style={{
-          position: 'fixed',
-          top: 0,
-        }}
-      >
-        <form className="form" onSubmit={this.handleSubmit}>
-          <button type="submit" className="button">
-            <span className="button-label">Search</span>
-          </button>
-          <input
-            className="input"
-            type="text"
-            //   autocomplete="off"
-            //   autofocus
-            placeholder="Search images and photos"
-            value={this.inputValue}
-            onChange={this.handleChange}
-          />
-        </form>
-      </header>
-    );
-  }
-}
+  return (
+    <header
+      className="searchbar"
+      style={{
+        position: 'fixed',
+        top: 0,
+      }}
+    >
+      <form className="form" onSubmit={handleSubmit}>
+        <button type="submit" className="button">
+          <span className="button-label">Search</span>
+        </button>
+        <input
+          className="input"
+          type="text"
+          //   autocomplete="off"
+          //   autofocus
+          placeholder="Search images and photos"
+          value={inputValue}
+          onChange={handleChange}
+        />
+      </form>
+    </header>
+  );
+};
+SearchForm.propTypes = {
+  onFormSubmit: PropTypes.func.isRequired,
+};
